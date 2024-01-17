10e33b2c00eea4c53bf39b5420f8e7fedba2f7dd
A.M <113894011+Amir800S@users.noreply.github.com>
2024-01-17 18:08:26 +0300

    homework

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from http import HTTPStatus
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+
+from send_email import send_confirmation_email
+from .serializers import RegisterSerializer
+
+
+User = get_user_model()
+
+class RegistrationsView(APIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        if user:
+            try:
+                send_confirmation_email(user.email, user.activation_code)
+            except:
+                return Response(
+                    {
+                        'message': 'Че то не то, на почте нет ниче',
+                        'data': serializer.data
+                    }, status=HTTPStatus.CREATED
+                )
+            return Response(serializer.data, status=HTTPStatus.CREATED)
+
+class ActivationView(APIView):
+    def get(self, request):
+        code = request.query_params.get('u')
+        user = get_object_or_404(User, activation_code=code)
+        user.is_active = True
+        user.activation_code = ''
+        user.save()
+        return Response('Активирован', status=HTTPStatus.OK)
+
+class ActivationPostView(APIView):
+    def post(self, request):
+        activation_code = request.data.get('activation_code')
+        if not activation_code:
+            return Response({
+                'error': 'Активационный код обязателен'},
+                status=HTTPStatus.BAD_REQUEST
+            )
+        user = get_object_or_404(User, activation_code=activation_code)
+        user.is_active = True
+        user.activation_code = ''
+        user.save()
+        return Response({
+            'message': 'Пользователь успешно активирован'},
+            status=HTTPStatus.OK
+        )
